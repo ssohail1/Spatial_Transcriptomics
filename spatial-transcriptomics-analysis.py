ec33a95b7a022adbdc611b0dd726fa26d3ef4f39
Sidra <80551186+ssohail1@users.noreply.github.com>
2025-05-07 16:29:14 -0500

    Update spatial-transcriptomics-analysis.py

--- spatial-transcriptomics-analysis.py
+++ spatial-transcriptomics-analysis.py
@@ -94,9 +94,150 @@ sc.tl.leiden(adata, resolution=0.8)
 ## used for visualisation 
 sc.tl.umap(adata)
 
-# Visualize marker genes through UMAP
-sc.pl.umap(adata, color=['leiden', 'CD19', 'MS4A1', 'FCER2'], cmap = 'Reds')
+# Visualizing marker genes
+sc.pl.umap(adata, color=['leiden', 'CD19', 'MS4A1', 'FCER2'], cmap = 'Blues')
+sc.pl.umap(adata, color=['leiden', 'CD19', 'FAM138A', 'OR4F5'], cmap = 'Blues')
+adata.var
+adata.obs
+
+# histology image of tissue section provided by Visium
+img = adata.uns['spatial']['V1_Human_Lymph_Node']['images']['hires']
+print(f"Shape: {img.shape}, Type: {type(img)}, Dtype: {img.dtype}")
+
+# First top left pixels
+img[:3, :3, :]
+
+sq.pl.spatial_scatter(adata)
+sq.pl.spatial_scatter(
+    adata,
+    color="total_counts",
+    size=1.5
+)
+
+# image container
+img = sq.im.ImageContainer.from_adata(adata)
+
+# adding "image_features" to data.obsm
+sq.im.calculate_image_features(
+    adata,
+    img,
+    features="summary",
+    key_added="image_features"
+)
+adata
+
+# Transfer to adata.obs for graphing
+adata.obs["summary_ch-0_mean"] = adata.obsm["image_features"]["summary_ch-0_mean"]
+sq.pl.spatial_scatter(
+    adata,
+    color="summary_ch-0_mean",
+    size=1.5
+)
+
+# clustering on image features
+sc.pp.neighbors(adata, use_rep="image_features")
+sc.tl.leiden(adata, key_added="image_features_clusters", resolution = 0.1)
+
+#adata.uns.pop('image_features_clusters_colors')
+sq.pl.spatial_scatter(
+    adata,
+    color="image_features_clusters",
+    size=1.5
+)
+
+# Extract spatial coordinates
+coords = adata.obsm["spatial"]
+print(f"Shape: {coords.shape}, Type: {type(coords)}, Dtype: {coords.dtype}")
+
+# First few coordinates
+coords[:3, :]
+
+# spatial neighborhood graph
+sq.gr.spatial_neighbors(adata, coord_type="grid") # graph is stored in adata.obsp["spatial_connectivities"]
+adata.obsp["spatial_connectivities"].shape # outputs that it is a 2D array/matrix in rows x columns format
+
+# visualize graph to see how spots are linked
+sq.pl.spatial_scatter(adata, connectivity_key="spatial_connectivities")
+
+# visualize expression of specific genes directly on histology image of tissue
+# to see how specific genes are spatially spread out and how it relates to tissue structure
+# CD3E: CD3 epsilon subunit of the T-cell receptor complex
+# crucial protein involved in T-cell development and function
+sq.pl.spatial_scatter(adata, color = 'CD3E')
+adata.obs.sort_values(by="n_genes", ascending=False)
+
+# IGKC: human gene that encodes constant domain of the kappa light chain of Abs
+sq.pl.spatial_scatter(adata, color = 'IGKC')
+
+# EEF1A1: eukaryotic translation elongation factor 1 alpha 1
+# protein with crucial role in protein synthesis
+sq.pl.spatial_scatter(adata, color = 'EEF1A1')
+adata.var.sort_values(by="log1p_mean_counts", ascending=False)
+
+# spatial map of gene expression clusters through Leiden
+sq.pl.spatial_scatter(adata, color = 'leiden')
+
+# look at marker genes of specific clusters
+sc.tl.rank_genes_groups(adata, 'leiden')
+sc.pl.rank_genes_groups(adata, n_genes=25, groups = ['2'])
+sc.pl.rank_genes_groups(adata, n_genes=25, groups = ['9']) # interferon-induced genes
+
+# visualize OAS1 gene on tissue to see its spatial location and gene expression activity 
+# OAS1 I s known interferon-stimulated gene
+sq.pl.spatial_scatter(adata, color = ['OAS1'])
+
+# identify genes with spatial structure - genes with expression levels non-randomly 
+# distributed across tissue with Moran's I = spatial autocorrelation measure where ↑ Moran's I
+# is expression is non-random
+sq.gr.spatial_autocorr(adata, mode="moran")
+
+# Top spatially structured genes
+adata.uns["moranI"]["I"].sort_values(ascending = False)[0:15]
+
+# visualize some of the top spatially structured genes
+sq.pl.spatial_scatter(adata, color = ['IGKC','IGHG2','IGHG4'])
+
+# take principal components (PCs) from PCA of gene count matrix 
+# and concatenate them with spatial coordinates of each spot
+# Extract PCA and spatial coordinates
+X_pca = adata.obsm["X_pca"][:, :10]  # use first 10 PCs
+X_spatial = adata.obsm["spatial"]
+
+# Scale values as the spatial coordinates have much larger magnitudes than the PCs
+scaler_pca = StandardScaler()
+X_pca_scaled = scaler_pca.fit_transform(X_pca)
+
+scaler_spatial = StandardScaler()
+X_spatial_scaled = scaler_spatial.fit_transform(X_spatial)
+
+# Combine scaled spatial and scaled pea data into one matrix
+X_combined = np.concatenate([X_pca_scaled, X_spatial_scaled], axis=1)
+adata.obsm["X_expr_spatial_scaled"] = X_combined
+
+# Build neighbor graph of combined scaled data and cluster using combined features with Leiden
+sc.pp.neighbors(adata, use_rep="X_expr_spatial_scaled")
+sc.tl.leiden(adata, key_added="leiden_expr_spatial_scaled", resolution = 0.5)
+
+# Visualize the combined data coloring the scaled combined Leiden data
+sq.pl.spatial_scatter(adata, color="leiden_expr_spatial_scaled", size=1.5)
 
 
+# repeat with top 20 PCs
+X_pca = adata.obsm["X_pca"][:, :20]  # use first 20 PCs
+X_spatial = adata.obsm["spatial"]
 
+scaler_pca = StandardScaler()
+X_pca_scaled = scaler_pca.fit_transform(X_pca)
 
+scaler_spatial = StandardScaler()
+X_spatial_scaled = scaler_spatial.fit_transform(X_spatial)
+
+X_combined = np.concatenate([X_pca_scaled, X_spatial_scaled], axis=1)
+adata.obsm["X_expr_spatial_scaled"] = X_combined
+
+sc.pp.neighbors(adata, use_rep="X_expr_spatial_scaled")
+sc.tl.leiden(adata, key_added="leiden_expr_spatial_scaled", resolution = 0.5)
+
+#sq.pl.spatial_scatter(adata, color="leiden_expr_spatial_scaled", size=1.0)
+
+adata.uns
